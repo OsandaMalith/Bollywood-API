bf748eedaf012ef1ff0c6b142e8af66d570cb5a5
Tushar Soni <tusharsoni1205@gmail.com>
2013-12-25 13:55:16 +0000

    update activity schema and db settings

--- activity.php
+++ activity.php
@@ -4,36 +4,35 @@ require_once("common.php");
 
 function postActivityData($userid, $data)
 {
-	global $link;
-	
-	foreach($data as $activity)
-	{
-		$put = $link->prepare("INSERT INTO activity (UserID, SongID, Action, `Timestamp`, Extra, Origin) VALUES (?,?,?,?,?,?)");
-		$put->bind_param("iisiss", $userid, $activity["SongID"], $activity["Action"], $activity["Timestamp"], $activity["Extra"], $activity["Origin"]);
-		$put->execute();
-		$put->close();
-	}
-	
-	message("Success");
+        global $link;
+
+        foreach($data as $activity)
+        {
+                $put = $link->prepare("INSERT INTO activity (UserID, SongID, Action, `Timestamp`, Extra) VALUES (?,?,?,?,?)");
+                $put->bind_param("iisis", $userid, $activity["SongID"], $activity["Action"], $activity["Timestamp"], $activity["Extra"]);
+                $put->execute();
+                $put->close();
+        }
+        
+        message("Success");
 }
 
 function getUserActivity($userid)
 {
-	global $link;
+        global $link;
 
-	$activity = array();
+        $activity = array();
 
-	$get = $link->prepare("SELECT ActivityID, Origin, SongID, `Timestamp`, Action, Extra FROM activity WHERE UserID=? LIMIT 20");
-	$get->bind_param("i", $userid);
-	$get->execute();
+        $get = $link->prepare("SELECT ActivityID, SongID, `Timestamp`, Action, Extra FROM activity WHERE UserID=? LIMIT 20");
+        $get->bind_param("i", $userid);
+        $get->execute();
 
-	bindArray($get, $row);
-	while($get->fetch())
-		$activity[] = arrayCopy($row);
+        bindArray($get, $row);
+        while($get->fetch())
+                $activity[] = arrayCopy($row);
 
-	$get->close();
+        $get->close();
 
-	return $activity;
+        return $activity;
 }
-
 ?>
