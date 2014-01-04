6f87ffbcf97404ac6aa3f2a29eb96d287484e125
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-04 15:57:24 +0000

    manages albumart. uses mapped data. searches all tables

--- albums.php
+++ albums.php
@@ -2,11 +2,11 @@
 require_once("common.php");
 
 
-function getAlbum($albumid)
+function getAlbum($albumid, $table = "songspk")
 {
 	global $link;
 
-	$album = $link->prepare("SELECT * FROM albums WHERE AlbumID=?");
+	$album = $link->prepare("SELECT AlbumID, Name, Cast, MusicDirector, Year, AlbumArt FROM ".$table."_albums WHERE AlbumID=?");
 	$album->bind_param("i", $albumid);
 	$album->execute();
 
@@ -21,36 +21,112 @@ function getAlbum($albumid)
 	if ($row["MusicDirector"] != "")
 		$row["MusicDirector"] = json_decode($row["MusicDirector"]);
 
+	$row["Provider"] = $table;
+	
+	$row = setAlbumArt($row, $table);
+	if ($table == "songspk")
+		$row = getMappedData($row);
+	
 	return $row;
 }
 
-function getAlbumWithSongs($albumid)
+function setAlbumArt($album, $table)
 {
-	global $link;
+	$album["AlbumArtBig"] = $album["AlbumArt"];
+	$album["AlbumArtSmall"] = $album["AlbumArt"];
 
-	$album = getAlbum ($albumid);
+	if ($table == "dhingana")
+	{
+		$album["AlbumArtBig"] = str_replace("148", "640", $album["AlbumArt"]);
+		$album["AlbumArtSmall"] = str_replace("148", "300", $album["AlbumArt"]);
+	}
+	else if($table == "saavn")
+	{
+		$album["AlbumArtBig"] = str_replace("150", "500", $album["AlbumArt"]);
+		$album["AlbumArtSmall"] = str_replace("150", "500", $album["AlbumArt"]);
+	}
 
-	$album["Songs"] = getSongsFromAlbum($albumid);
+	unset($album["AlbumArt"]);
 
 	return $album;
 }
 
-function searchAlbumName($name)
+function getMappedData($album)
 {
 	global $link;
 
+	$map = $link->prepare("SELECT SaavnID FROM map WHERE PKID=?");
+	$map->bind_param("i", $album["AlbumID"]);
+	$map->execute();
+	$map->store_result();
+	if ($map->num_rows == 0)
+	{	
+		$map->close();
+		return $album;	
+	}
+	bindArray($map, $row);
+	$map->fetch();
+	$saavnid = $row["SaavnID"];
+	$map->close();
+
+	$newalbum = getAlbum($saavnid, "saavn");
+	$album["Name"] = $newalbum["Name"];
+	$album["Cast"] = $newalbum["Cast"];
+	$album["Year"] = $newalbum["Year"];
+	$album["MusicDirector"] = $newalbum["MusicDirector"];
+	$album["AlbumArt"] = $newalbum["AlbumArtSmall"];
+	$album["Provider"] = "saavn";
+	$album = setAlbumArt($album, "saavn");
+
+	return $album;	
+}
+
+function getAlbumWithSongs($albumid, $table = "songspk")
+{
+	global $link;
+
+	$album = getAlbum ($albumid, $table);
+
+	$album["Songs"] = getSongsFromAlbum($albumid, $table);
+
+	return $album;
+}
+
+function searchAlbumNameInAll($name, $isFinal)
+{
+	$pk = searchAlbumName($name, $isFinal, "songspk");
+	if (count($pk) == 0)
+		return searchAlbumName($name, $isFinal, "dhingana");
+	else
+		return $pk;
+}
+
+function searchAlbumName($name, $isFinal, $table = "songspk")
+{
+	global $link;
+	
 	$albums = array();
 
-	$name = $name."%";
-	
-	$search = $link->prepare("SELECT AlbumID FROM albums WHERE Name LIKE ? ORDER BY Year DESC LIMIT 10");
-	$search->bind_param("s", $name);
+	$fuzzy = "damlev(Name, ?)";
+	if ($isFinal)
+	{
+		$query = "SELECT AlbumID FROM ".$table."_albums WHERE $fuzzy <= 4 ORDER BY $fuzzy ASC LIMIT 10";
+		$search = $link->prepare($query);
+		$search->bind_param("ss", $name, $name);
+	}
+	else
+	{
+		$startWith = $name."%"; 
+		$query = "SELECT AlbumID FROM ".$table."_albums WHERE $fuzzy <=2 OR Name Like ? LIMIT 10";	
+		$search = $link->prepare($query);
+		$search->bind_param("ss", $name, $startWith);
+	}
 	$search->execute();
 	$search->store_result();
 
 	bindArray($search, $row);
 	while($search->fetch())
-		array_push($albums, getAlbumWithSongs($row["AlbumID"]));
+		array_push($albums, getAlbumWithSongs($row["AlbumID"], $table));
 
 	$search->free_result();
 	$search->close();
