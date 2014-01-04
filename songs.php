1365f9f44624051c57be60bf960c17e9310fd116
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-04 15:59:42 +0000

    uses data from all tables

--- songs.php
+++ songs.php
@@ -2,49 +2,51 @@
 require_once("common.php");
 
 
-function getSong($songid)
+function getSong($songid, $table = "songspk")
 {
 	global $link;
 
-	$album = $link->prepare("SELECT SongID,AlbumID,Name,Singers,Mp3 FROM songs WHERE SongID=?");
-	$album->bind_param("i", $songid);
-	$album->execute();
+	$song = $link->prepare("SELECT SongID,AlbumID,Name,Singers,Mp3 FROM ".$table."_songs WHERE SongID=?");
+	$song->bind_param("i", $songid);
+	$song->execute();
 
-	bindArray($album, $row);
-	$album->fetch();
+	bindArray($song, $row);
+	$song->fetch();
 
-	$album->close();
+	$song->close();
 
 	if ($row["Singers"] != "")
 		$row["Singers"] = json_decode($row["Singers"]);
 
+	$row["Provider"] = $table;
+
 	return $row;
 }
 
-function getSongWithAlbum($songid)
+function getSongWithAlbum($songid, $table = "songspk")
 {
 	global $link;
 
-	$song = getSong($songid);
+	$song = getSong($songid, $table);
 	
-	$song["Album"] = getAlbum($song["AlbumID"]);
+	$song["Album"] = getAlbum($song["AlbumID"], $table);
 
 	return $song;
 }
 
-function getSongsFromAlbum($albumid)
+function getSongsFromAlbum($albumid, $table = "songspk")
 {
 	$response = array();
 	global $link;
 
-	$songs = $link->prepare("SELECT SongID FROM songs WHERE AlbumID=?");
+	$songs = $link->prepare("SELECT SongID FROM ".$table."_songs WHERE AlbumID=?");
 	$songs->bind_param("i", $albumid);
 	$songs->execute();
 	$songs->store_result();
 
 	bindArray($songs, $row);
 	while($songs->fetch())
-		array_push($response, getSong($row["SongID"]));
+		array_push($response, getSong($row["SongID"], $table));
 
 	$songs->free_result();
 	$songs->close();
@@ -52,22 +54,41 @@ function getSongsFromAlbum($albumid)
 	return $response;
 }
 
-function searchSongName($name)
+function searchSongNameInAll($name, $isFinal)
+{
+	$pk = searchSongName($name, $isFinal, "songspk");
+	if (count($pk) == 0)
+		return searchSongName($name, $isFinal, "dhingana");
+	else
+		return $pk;
+}
+
+function searchSongName($name, $isFinal, $table = "songspk")
 {
 	global $link;
 
 	$songs = array();
 
-	$name = $name."%";
-	
-	$search = $link->prepare("SELECT SongID FROM songs WHERE Name LIKE ? LIMIT 15");
-	$search->bind_param("s", $name);
+	$fuzzy = "damlev(Name, ?)";	
+        if ($isFinal)
+        {
+                $query = "SELECT SongID FROM ".$table."_songs WHERE $fuzzy <= 4 ORDER BY $fuzzy ASC LIMIT 10";
+                $search = $link->prepare($query);
+                $search->bind_param("ss", $name, $name); 
+	}
+        else
+        {
+                $startWith = $name."%";
+                $query = "SELECT SongID FROM ".$table."_songs WHERE $fuzzy <=2 OR Name Like ? LIMIT 10";   
+                $search = $link->prepare($query);
+                $search->bind_param("ss", $name, $startWith);
+        }
 	$search->execute();
 	$search->store_result();
 
 	bindArray($search, $row);
 	while($search->fetch())
-		array_push($songs, getSongWithAlbum($row["SongID"]));
+		array_push($songs, getSongWithAlbum($row["SongID"], $table));
 
 	$search->free_result();
 	$search->close();
