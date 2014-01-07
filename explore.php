4020b2cb1f52b59629dc4009a34e7116e2b3ce40
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-07 08:58:51 +0000

    update explore.php

--- explore.php
+++ explore.php
@@ -5,13 +5,10 @@ require_once("common.php");
 function getExploreAll()
 {
 	
-	$titles = array("Latest", "Popular", "Old", "Random");
+	$titles = array("Latest");
         $latest = array(3023, 979, 1172, 2825, 102, 2922, 2394);
-	$popular = array();
-	$old = array();
-	$random = array();       
 
-	$all = array("Latest" => albumsForExplore($latest), "Popular" => albumsForExplore($popular), "Old" => albumsForExplore($old),"Random" => albumsForExplore($random));
+	$all = array("Latest" => albumsForExplore($latest));
         
         return array("Titles" => $titles, "Albums" => $all);    
 }
