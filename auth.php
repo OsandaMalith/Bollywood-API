2b4b6b81533e5cea22d8e336e5ac31b4b342ebf3
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-07 20:58:30 +0000

    runs the app only on valid requests

--- auth.php
+++ auth.php
@@ -1,6 +1,47 @@
 <?php
 require_once("common.php");
 
+function createDeveloper($email)
+{
+	global $link;
+
+	if (doesDeveloperExist($email) == true)
+	{
+		message("Developer already exists with this email");
+		return;
+	}
+	
+	$devID = hash("crc32", $email);
+	$privateKey = hash("md5", $email.time().generateRandomString());
+
+	$insert = $link->prepare("INSERT INTO developers (DeveloperID, PrivateKey, Email) VALUES (?,?,?)");
+	$insert->bind_param("sss", $devID, $privateKey, $email);
+	$insert->execute();
+	$insert->close();
+	
+
+	echo $devID."    ".$privateKey;
+}
+
+function doesDeveloperExist($email)
+{
+	global $link;
+	$exists = false;
+
+	$select = $link->prepare("SELECT * FROM developers WHERE Email=?");
+	$select->bind_param("s", $email);
+	$select->execute();
+	$select->store_result();
+
+	if ($select->num_rows > 0)
+		$exists = true;
+
+	$select->free_result();
+	$select->close();
+
+	return $exists;
+}
+
 function getPrivateKey($developerID)
 {
 	global $link;
@@ -8,21 +49,48 @@ function getPrivateKey($developerID)
 	$select = $link->prepare("SELECT PrivateKey FROM developers WHERE DeveloperID=?");
 	$select->bind_param("s", $developerID);
 	$select->execute();
+	$select->store_result();
 
 	if ($select->num_rows == 0)
-		message("Invalid Developer ID");
-
-	bindArray($select, $key);
-	$select->fetch();
+		$key = -1;
+	else
+	{
+		bindArray($select, $row);
+		$select->fetch();
+		$key = $row["PrivateKey"];
+	}
+	
+	$select->free_result();
 	$select->close();
 
 	return $key;
 }
 
-function validateRequest($developerID, $timestamp, $hmac, $request)
+function validateRequest($developerID, $timestamp, $user_hmac)
 {
-//	$privateKey = 
+	if (time() - $timestamp > 3000)
+	{
+		message("Request is too old");
+		return false;
+	}
 
+	$privateKey = getPrivateKey($developerID);
+
+	if ($privateKey == -1)
+	{
+		message("Invalid Developer ID");
+		return false;
+	}
+
+	$valid_hmac = hash_hmac("sha256", $timestamp, $privateKey);
+
+	if ($valid_hmac != $user_hmac)
+	{
+		message("Invalid hmac");
+		return false;
+	}
+
+	return true;
 }
 
 ?>
