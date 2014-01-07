598ede8b471c4ea1bf5596958662d9077151d542
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-07 19:44:07 +0100

    remove unnecessary files

--- index.php
+++ index.php
@@ -11,20 +11,6 @@ $app->get('/', function () {
  	message("Hello World!"); 	
  });
 
-$app->get('/user/create', function() {
-	echo json_encode(createNewUser());
-});
-
-$app->get("/user/:userid/activity", function($userid) {
-	echo json_encode(getUserActivity($userid));
-});
-
-$app->post("/user/:userid/activity", function($userid) {
-	global $app;
-	$activityData = json_decode($app->request->getBody(), true);
-	postActivityData($userid, $activityData["data"]);
-});
-
 $app->get("/albums/:albumid", function($albumid) {
 	echo json_encode(getAlbum($albumid));
 });
@@ -56,34 +42,6 @@ $app->get("/search/songs/:name", function($name) {
 $app->get("/search/like/songs/:name", function($name) {
 	echo json_encode(searchSongNameInAll($name, false));
 });
-
-$app->get("/explore", function() {	
-	echo json_encode(getExploreAll());
-});
-
-/*
-$app->get("/playlist", function() {
-	echo json_encode(getPlaylist());
-});
-
-$app->post("/playlist", function() {
-	global $app;
-	$songids = $app->request->getBody();
-	updatePlaylistWithSongIDs($songids);
-});
-
-$app->post('/user/login', function() {
-	global $app;
-	$credentials = json_decode($app->request->getBody());
-	
-	if (login($credentials->userid, $credentials->password))
-		message("success");
-	else
-		message("failed");
-});
-*/
-
-//sleep((mt_rand()%3) + 1);
 
 $app->run();
 
