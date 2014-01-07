60e8aab111ddf1669d54edc473d02be038f040bc
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-07 09:41:44 +0000

    filters out duplicate activity before inserting

--- activity.php
+++ activity.php
@@ -11,16 +11,40 @@ function postActivityData($userid, $data)
 		message("Failed");
 		return;
 	}
-
+	
+	$partialSuccess = false;
         foreach($data as $activity)
         {
-                $put = $link->prepare("INSERT INTO activity (UserID, SongID, Action, `Timestamp`, Extra) VALUES (?,?,?,?,?)");
-                $put->bind_param("iisis", $userid, $activity["SongID"], $activity["Action"], $activity["Timestamp"], $activity["Extra"]);
-                $put->execute();
-                $put->close();
-        }
+		if (doesActivityExist($userid, $activity) == false)
+		{
+			$put = $link->prepare("INSERT INTO activity (UserID, SongID, Action, `Timestamp`, Extra) VALUES (?,?,?,?,?)");
+			$put->bind_param("iisis", $userid, $activity["SongID"], $activity["Action"], $activity["Timestamp"], $activity["Extra"]);
+			$put->execute();
+			$put->close();
+        	}
+		else
+			$partialSuccess = true;
+	}
         
-        message("Success");
+	if ($partialSuccess)
+		message("Success-Partial");
+	else
+        	message("Success");
+}
+
+function doesActivityExist($userid, $activity)
+{
+	global $link;
+	$check = $link->prepare("SELECT * FROM activity WHERE UserID=? and `Timestamp`=?");
+	$check->bind_param("ii", $userid, $activity["Timestamp"]);
+	$check->execute();
+	$check->store_result();
+	$count = $check->num_rows;
+	$check->free_result();
+	$check->close();
+	if ($count == 0)
+		return false;
+	return true;
 }
 
 function getUserActivity($userid)
