92eacd8bd3e0f9ef611f72c726050d71925beccd
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-07 20:33:13 +0100

    message for invalid dev id

--- auth.php
+++ auth.php
@@ -8,11 +8,21 @@ function getPrivateKey($developerID)
 	$select = $link->prepare("SELECT PrivateKey FROM developers WHERE DeveloperID=?");
 	$select->bind_param("s", $developerID);
 	$select->execute();
+
+	if ($select->num_rows == 0)
+		message("Invalid Developer ID");
+
 	bindArray($select, $key);
 	$select->fetch();
 	$select->close();
 
 	return $key;
+}
+
+function validateRequest($developerID, $timestamp, $hmac, $request)
+{
+//	$privateKey = 
+
 }
 
 ?>
