210877b68ffb1141bef2eb1a8d7e0b6ddaf8cbef
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-08 19:42:14 +0000

    searches using actual api call

--- v1/frontend_wrapper.php
+++ v1/frontend_wrapper.php
@@ -10,7 +10,13 @@ function signup()
 
 function search()
 {
-	echo json_encode(searchAlbumNameInAll($_GET["name"], true));
+	global $PRIVATEKEY, $DEVID;
+	$timestamp = time();
+	$hmac = hash_hmac("sha256", $timestamp, $PRIVATEKEY);
+
+	$url = "http://54.201.193.207/Bollywood-API/v1/index.php/search/albums/".rawurlencode($_GET["name"])."?DeveloperID=$DEVID&Timestamp=$timestamp&hmac=$hmac";
+	
+	echo file_get_contents("$url");
 }
 
 ?>
