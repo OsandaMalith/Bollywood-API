ff9b1da7946e064e6f20584b7d9a23dc6c00a44b
Tushar Soni <tusharsoni1205@gmail.com>
2014-01-21 10:29:45 +0000

    wrapper to use stats

--- v1/frontend_wrapper.php
+++ v1/frontend_wrapper.php
@@ -3,6 +3,18 @@ require_once("common.php");
 
 call_user_func($_GET["call"]);
 
+function genStats()
+{
+	$stat = new Statistics(constant($_GET["period"]),
+				$_GET["pcount"],
+				constant($_GET["action"]),
+				constant($_GET["origin"]),
+				constant($_GET["provider"]),
+				constant($_GET["cumulative"]));
+	$stat->fetchStats();
+	echo json_encode($stat);
+}
+
 function signup()
 {
 	$developer = new Developer($_GET["email"], "Email");
